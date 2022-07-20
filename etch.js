d013e520804b0c54388dd7ed7a90bc1a9eb046b5
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-20 14:48:35 -0400

    Cut run time

--- etch.js
+++ etch.js
@@ -1,41 +1,44 @@
 const etch = document.getElementById('container');
 
-function cellDraw(){
-    const cells = document.querySelectorAll('.cell');
+initialGrid();
 
-    cells.forEach(cell => {
-            cell.addEventListener('mouseover', function colorChange(event) {
-            cell.setAttribute('style', 'background-color:black;');
-            
-        })
-})
-}
-
-
-function createGrid(newSize){
-    for(let i=0; i < newSize*newSize; i++){
-        
-        let block = document.createElement('div');
-        block.classList.add('cell');
-        etch.appendChild(block);
-        cellDraw();
+function initialGrid () {
+    for(let i=0; i < 16*16; i++){
+    let block = document.createElement('div');
+    block.classList.add('cell');
+    etch.appendChild(block);
     }
+    cellDraw();
 }
 
 function cellChange() {
-    let newSize = prompt("How long should the rows/columns be?");
+    let newSize = prompt("How many rows and columns should there be?");
     etch.innerHTML = "";
     etch.style.gridTemplateColumns = "repeat("+ parseInt(newSize) +", auto)";
     createGrid(newSize);
 }
 
-for(let i=0; i < 16*16; i++){
-    
-    let block = document.createElement('div');
-    block.classList.add('cell');
-    etch.appendChild(block);
+function createGrid(newSize){
+    for(let i=0; i < newSize*newSize; i++){
+        let block = document.createElement('div');
+        block.classList.add('cell');
+        etch.appendChild(block);
+    }
     cellDraw();
 }
+
+function cellDraw(){
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+            cell.addEventListener('mouseover', function colorChange(event) {
+            cell.setAttribute('style', 'background-color:black;');
+        })
+})
+}
+
+
+
+
 
 
 
